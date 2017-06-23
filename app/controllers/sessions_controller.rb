40131b759e6e0ce7563348f2da795140195a0f26
Ryan Murphy <Murph@Ryans-MacBook-Pro-2.local>
2017-06-22 18:28:05 -0700

    add models for band, note, track and album

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
-class SessionsController < ApplicationController
+
+  class SessionsController < ApplicationController
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      flash[:errors] = ["Invalid credentials."]
+      render :new
+
+
+    elsif !user.activated?
+      redirect_to root_url, alert: "You must activate your account first! Check your email."
+    else
+      login_user!(user)
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+
+    redirect_to new_session_url
+  end
+
+  def new
+    render :new
+  end
 end
